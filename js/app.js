af5970295ee3aee13e3ca5f6f1ac0103750fc7fc
Hitoshi Honma <h.honma@jaftac.co.jp>
2015-05-10 18:37:22 +0900

    Change design pattern of the code

--- js/app.js
+++ js/app.js
@@ -1,22 +1,47 @@
 (function (global){
-	
+
 	global.app = {};
-	
-	function TodoModel(){
-		this.data = [];
-		this.add = function(newData){
-			this.data.push(newData);
-			this.trigger('add');
+
+	function Store(){
+		riot.observable(this);
+		this.tasks = [];
+		this.newTaskFormState = {
+			value : '',
+			isAddable : false
 		};
-		this.set = function(newData, index){
-			this.data[index] = newData;
+		this.add = function(key, value){
+			var target = _.isString(key) ? this[key] : key;
+			target.push(value);
 			this.trigger('change');
 		};
-		riot.observable(this);
+		this.change = function(key, value){
+			var target = _.isString(key) ? this[key] : key;
+			_.extend(target, value);
+			this.trigger('change');
+		};
 	};
-	global.app.todoModel = new TodoModel();
-	
+	global.app.store = new Store();
+
+	function TaskActions(store){
+		this.addTask = function(task){
+			store.add('tasks', task);
+			store.change('newTaskFormState', {
+				value : '',
+				isAddable : false
+			});
+		};
+		this.changeNewTaskFormState = function(state){
+			store.change('newTaskFormState', state);
+		};
+		this.toggleTask = function(index){
+			store.change(store.tasks[index], {
+				isDone : !store.tasks[index].isDone
+			});
+		};
+	};
+	global.app.taskActions = new TaskActions(app.store);
+
 	riot.settings.brackets = '{{ }}';
 	riot.mount('*');
-	
+
 }(window));
